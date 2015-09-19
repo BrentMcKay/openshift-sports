87bebb1b2b9dae5e2357fd3598b64ede2fad96c8
Brent McKay <brent.mckay@gmail.com>
2015-09-18 21:45:47 -0700

    packages

--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,12 @@ virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
 try:
     execfile(virtualenv, dict(__file__=virtualenv))
+    exec_namespace = dict(__file__=virtualenv)
+    with open(virtualenv, 'rb') as exec_file:
+        file_contents = exec_file.read()
+    compiled_code = compile(file_contents, virtualenv, 'exec')
+    exec(compiled_code, exec_namespace)
+
 except IOError:
     pass
 #
